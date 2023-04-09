826595d0ef2ed376e36ba02bf31b2b8219e80039
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2023-04-09 20:53:01 -0300

    addItem and removeItem

--- src/main/java/main/DynamicArray.java
+++ src/main/java/main/DynamicArray.java
@@ -1,7 +1,75 @@
 package main;
 
-public class DynamicArray {
+import java.util.Arrays;
 
+public class DynamicArray<E>  {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private E[] list;
+	
+	private int size = 0;
+	
+	private final int INITIAL_CAPACITY = 2;
+	
+	private int realSizeArrayList= 0;
+	
+	@SuppressWarnings("unchecked")
+	public DynamicArray() {
+		list = (E[]) new Object[INITIAL_CAPACITY];
+		realSizeArrayList = INITIAL_CAPACITY;
+	}
+
+	public void add(E i) {
+		controlCapacity();
+		list[size] = i;
+		size++;
+	}
+
+	private void controlCapacity() {
+		grow();
+	}
+
+	private void grow() {
+		if(size==list.length) {
+			 int newCapacity = list.length * 2;
+			 list = Arrays.copyOf(list, newCapacity);
+			 this.realSizeArrayList = newCapacity;
+		}
+	}
+
+	public E get(int index) {
+		validateIndex(index);
+		return list[index];
+	}
+
+	private void validateIndex(int index) {
+		if(index >= list.length) {
+			throw new RuntimeException("ArrayIndexOutOfBoundsException");
+		}
+	}
+
+	public int size() {
+		return size;
+	}
+	
+	public int getRealSize() {
+		return realSizeArrayList;
+	}
+
+	public E remove(int index_to_remove) {
+		validateIndex(index_to_remove);
+		
+		E valueToRemove = list[index_to_remove];
+		int numMoved = size - index_to_remove - 1;
+		
+		if (numMoved > 0)
+			System.arraycopy(list, index_to_remove+1, list, index_to_remove,numMoved);
+		
+		
+		 list[--size] = null;
+		 return  valueToRemove;
+	}
 	
 	
 }
--- src/test/java/main/DynamicArrayTest.java
+++ src/test/java/main/DynamicArrayTest.java
@@ -1,14 +1,42 @@
 package main;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class DynamicArrayTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addItem() {
+		DynamicArray<Integer> list = new DynamicArray<Integer>();
+		
+		assertEquals(list.size(), 0);
+		assertEquals(list.getRealSize(), 2);
+		
+		list.add(1);
+		assertEquals(list.size(), 1);
+		assertEquals(list.getRealSize(), 2);
+		
+		list.add(1);
+		assertEquals(list.size(), 2);
+		assertEquals(list.getRealSize(), 2);
+		
+		list.add(1);
+		assertEquals(list.size(), 3);
+		assertEquals(list.getRealSize(), 4);
+
+	}
+	
+	@Test
+	public void removeItem() {
+		DynamicArray<Integer> list = new DynamicArray<Integer>();
+		
+		list.add(10);
+		list.add(10);
+		list.add(10);
+		list.add(10);
+		list.remove(1);
+		assertEquals(list.size(), 3);
 	}
 
 }
